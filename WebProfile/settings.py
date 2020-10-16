a02294e864aae4e86eabcab349874763b10ff349
Sylvain <sylvain.rieutor@orange.fr>
2020-10-16 19:06:17 +0200

    fix postgres

--- WebProfile/settings.py
+++ WebProfile/settings.py
@@ -152,3 +152,5 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
