0f8408814bcfb3d9189c3f47a0bc4973e6b6b40e
Sylvain <sylvain.rieutor@orange.fr>
2020-10-18 14:29:48 +0200

    change ''outils' section

--- webapp/models.py
+++ webapp/models.py
@@ -31,7 +31,7 @@ class DevTool(models.Model):
     logo = models.CharField(max_length=50, blank=True, null=True)
 
     class Meta:
-        ordering = ['tool_type']
+        ordering = ['family', 'tool_type']
 
     def __str__(self):
         return self.name
